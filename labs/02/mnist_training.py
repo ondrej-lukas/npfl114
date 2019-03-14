Decay reate computation fixed

labs/02/mnist_training.py
@@ -45,7 +45,6 @@ args.logdir = os.path.join("logs","{}-{}-{}".format(
 mnist = MNIST()
 
 number_of_batches = args.epochs*(len(mnist.train.data['labels'])/args.batch_size)
-
 # Create the model
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=[MNIST.H, MNIST.W, MNIST.C]),
@@ -73,12 +72,10 @@ optimizer = None
 if args.optimizer == "Adam":
     if args.decay:
         if args.decay == "exponential":
-            rate = np.exp(np.log(args.learning_rate_final/args.learning_rate)/(args.epochs*args.batch_size-1))
-            print("rate", rate, " no_batches ", number_of_batches)
             learning_rate_fn = tf.keras.optimizers.schedules.ExponentialDecay(
                 args.learning_rate,
-                args.batch_size,
-                decay_rate=0.9)
+                decay_steps=number_of_batches,
+                decay_rate=args.learning_rate_final/args.learning_rate)
         elif args.decay == "polynomial":
             learning_rate_fn = tf.keras.optimizers.schedules.PolynomialDecay(
             args.learning_rate,
@@ -93,12 +90,11 @@ if args.optimizer == "Adam":
 elif args.optimizer == "SGD":
     if args.decay:
         if args.decay == "exponential":
-            rate = np.exp(np.log(args.learning_rate_final/args.learning_rate)/(args.epochs*args.batch_size-1))
-            print("rate", rate)
+            #print("LRrate", float(rate))
             learning_rate_fn = tf.keras.optimizers.schedules.ExponentialDecay(
                 args.learning_rate,
-                args.batch_size,
-                decay_rate=0.9)
+                decay_steps=number_of_batches,
+                decay_rate=args.learning_rate_final/args.learning_rate)
         elif args.decay == "polynomial":
             learning_rate_fn = tf.keras.optimizers.schedules.PolynomialDecay(
                 args.learning_rate,