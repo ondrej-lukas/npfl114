190035a7af729e22918b0692066a5da17ea2c28f
Ondrej Lukas <ondrej.lukas95@gmail.com>
2019-03-15 17:02:36 +0100

    2 neurons in the last layer, fixed input layer so loss is working properly

--- labs/02/gym_cartpole.py
+++ labs/02/gym_cartpole.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+#bfc95faa-444e-11e9-b0fd-00505601122b
+#3da961ed-4364-11e9-b0fd-00505601122b
 import argparse
 import datetime
 import os
@@ -11,7 +13,7 @@ import tensorflow as tf
 # TODO: Set reasonable defaults and possibly add more arguments.
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=50, type=int, help="Number of epochs.")
+parser.add_argument("--epochs", default=250, type=int, help="Number of epochs.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 args = parser.parse_args()
 
@@ -22,30 +24,30 @@ tf.config.threading.set_inter_op_parallelism_threads(args.threads)
 tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
 # Create logdir name
-# args.logdir = "logs/{}-{}-{}".format(
-#     os.path.basename(__file__),
-#     datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-#     ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
-# )
-
+args.logdir = os.path.join("logs","{}-{}-{}".format(
+    os.path.basename(__file__),
+    datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+    ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+))
 # Load the data
 observations, labels = [], []
 with open("gym_cartpole-data.txt", "r") as data:
     for line in data:
         columns = line.rstrip("\n").split()
         observations.append([float(column) for column in columns[0:-1]])
-        labels.append(int(columns[-1]))
+        labels.append(float(columns[-1]))
 observations, labels = np.array(observations), np.array(labels)
 
 # TODO: Create the model in the `model` variable.
 # # However, beware that there is currently a bug in Keras which does
 # # not correctly serialize InputLayer. Instead of using an InputLayer,
 # # pass explicitly `input_shape` to the first real model layer.
-model = None
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(32, activation=tf.nn.relu),
-    tf.keras.layers.Dense(8, activation=tf.nn.relu),
-    tf.keras.layers.Dense(1, activation=tf.nn.softmax)]
+    tf.keras.layers.Dense(4, activation=tf.nn.relu, input_shape=(4,)),
+    tf.keras.layers.Dense(20, activation=tf.nn.relu),
+    tf.keras.layers.Dense(80, activation=tf.nn.relu),
+    tf.keras.layers.Dense(40, activation=tf.nn.relu),
+    tf.keras.layers.Dense(2, activation=tf.nn.softmax)]
 )
 
 model.compile(
@@ -56,5 +58,8 @@ model.compile(
 
 # tb_callback=tf.keras.callbacks.TensorBoard(args.logdir)
 model.fit(observations, labels, batch_size=args.batch_size, epochs=args.epochs)#, callbacks=[tb_callback])
+
+
+
 
 model.save("gym_cartpole_model.h5", include_optimizer=False)
