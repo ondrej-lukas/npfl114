2384fc6feff736eaca1c387dece4a67344251fd0
Michaela Urbanovská <urbanm30@fel.cvut.cz>
2019-04-11 20:18:38 +0200

    Fashion masks network added

--- labs/05/fashion_masks.py
+++ labs/05/fashion_masks.py
@@ -4,7 +4,6 @@
 import numpy as np
 import tensorflow as tf
 
-
 from fashion_masks_data import FashionMasks
 
 # TODO: Define a suitable model in the Network class.
@@ -13,10 +12,49 @@ from fashion_masks_data import FashionMasks
 # the label and the other one the masks.
 class Network:
     def __init__(self, args):
-        pass
+        model = tf.keras.Sequential()
+        input_shape = [28,28,1]
+
+        # first CONV => RELU => CONV => RELU => POOL layer set
+        model.add(tf.keras.layers.Conv2D(32, (3, 3), padding="same",
+                         input_shape=input_shape))
+        model.add(tf.keras.layers.Activation("relu"))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Conv2D(32, (3, 3), padding="same"))
+        model.add(tf.keras.layers.Activation("relu"))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+        model.add(tf.keras.layers.Dropout(0.25))
+
+        # second CONV => RELU => CONV => RELU => POOL layer set
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), padding="same"))
+        model.add(tf.keras.layers.Activation("relu"))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), padding="same"))
+        model.add(tf.keras.layers.Activation("relu"))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+        model.add(tf.keras.layers.Dropout(0.25))
+
+        # first (and only) set of FC => RELU layers
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(512))
+        model.add(tf.keras.layers.Activation("relu"))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.Dropout(0.5))
+
+        # softmax classifier
+        model.add(tf.keras.layers.Dense(10))
+        model.add(tf.keras.layers.Activation("softmax"))
+
+        optimizer = tf.keras.optimizers.Adam()
+        loss = tf.keras.losses.SparseCategoricalCrossentropy()
+        metrics = ['accuracy']
+        model.compile(loss=loss,optimizer=optimizer,metrics=metrics)
+        self.model = model
 
     def train(self, fashion_masks, args):
-        pass
+        self.model.fit(fashion_masks.train.data['images'][0:1000],fashion_masks.train.data['labels'][0:1000], batch_size=args.batch_size, epochs=args.epochs)
 
 if __name__ == "__main__":
     import argparse
@@ -26,9 +64,9 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
-    parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
-    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
+    parser.add_argument("--epochs", default=1, type=int, help="Number of epochs.")
+    parser.add_argument("--threads", default=0, type=int, help="Maximum number of threads to use.")
     args = parser.parse_args()
 
     # Fix random seeds
@@ -52,9 +90,16 @@ if __name__ == "__main__":
     network.train(fashion_masks, args)
 
     # Predict test data in args.logdir
-    with open(os.path.join(args.logdir, "fashion_masks_test.txt"), "w", encoding="utf-8") as out_file:
+    with open("fashion_masks_test.txt", "w", encoding="utf-8") as out_file:
         # TODO: Predict labels and masks on fashion_masks.test.data["images"],
         # into test_labels and test_masks (test_masks is assumed to be
         # a Numpy array with values 0/1).
+        test_labels = network.model.predict(fashion_masks.test.data['images'])
+        test_masks = []
+        for i in range(0,test_labels.shape[0]):
+            max_class = max(test_labels[i,:])
+            mask = np.zeros((28,28,1))
+            mask[fashion_masks.test.data['images'][i] > 0.5] = 1
+
         for label, mask in zip(test_labels, test_masks):
             print(label, *mask.astype(np.uint8).flatten(), file=out_file)
