37ba19a6ecacfbd8544c1aede68156d30ed7f961
Ondrej Lukas <ondrej.lukas95@gmail.com>
2019-04-30 16:38:09 +0200

    first working attempt

--- labs/08/3d_recognition.py
+++ labs/08/3d_recognition.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+#bfc95faa-444e-11e9-b0fd-00505601122b
+#3da961ed-4364-11e9-b0fd-00505601122b
+
 import numpy as np
 import tensorflow as tf
 
@@ -9,19 +12,44 @@ class Network:
     def __init__(self, modelnet, args):
         # TODO: Define a suitable model, and either `.compile` it, or prepare
         # optimizer and loss manually.
+        inputs = tf.keras.layers.Input(shape=[args.modelnet, args.modelnet, args.modelnet,1])
+        hidden = tf.keras.layers.Conv3D(filters=32,kernel_size=(5,5,5), strides=(1,1,1), padding="same" ,activation=None)(inputs)
+        hidden = tf.keras.layers.BatchNormalization()(hidden)
+        hidden = tf.keras.activations.relu(hidden)
+        hidden = tf.keras.layers.MaxPool3D(pool_size=(2, 2, 2), strides=(1,1,1),padding="same")(hidden)
+        hidden = tf.keras.layers.Conv3D(filters=32,kernel_size=(3,3,3), strides=(1,1,1), padding="same" ,activation=None)(inputs)
+        hidden = tf.keras.layers.BatchNormalization()(hidden)
+        hidden = tf.keras.activations.relu(hidden)
+        hidden = tf.keras.layers.MaxPool3D(pool_size=(2, 2, 2), strides=(1,1,1),padding="same")(hidden)
+        hidden = tf.keras.layers.Conv3D(filters=64,kernel_size=(3,3,3), strides=(1,1,1), padding="same" ,activation=None)(inputs)
+        hidden = tf.keras.layers.BatchNormalization()(hidden)
+        hidden = tf.keras.activations.relu(hidden)
+        hidden = tf.keras.layers.Flatten()(hidden)
+        outputs = tf.keras.layers.Dense(len(modelnet.LABELS), activation="softmax")(hidden)
+        self.model = tf.keras.Model(inputs=inputs, outputs=outputs)
+        adam = tf.keras.optimizers.Adam()
+        self.model.compile(
+            optimizer=adam,
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+        )
 
         self.tb_callback=tf.keras.callbacks.TensorBoard(args.logdir, update_freq=1000, profile_batch=1)
         self.tb_callback.on_train_end = lambda *_: None
 
     def train(self, modelnet, args):
         # TODO: Train the network on a given dataset.
-        raise NotImplementedError()
+        self.model.fit(modelnet.train.data["voxels"], modelnet.train.data["labels"],
+        batch_size=args.batch_size, epochs=args.epochs,
+        validation_data=(modelnet.dev.data["voxels"], modelnet.dev.data["labels"]),
+        callbacks=[self.tb_callback],)
+        #raise NotImplementedError()
 
     def predict(self, dataset, args):
         # TODO: Predict method should return a list/np.ndarray of
         # label probabilities from the test set
-        raise NotImplementedError()
-
+        #raise NotImplementedError()
+        return self.model.predict(dataset.data["voxels"])
 
 if __name__ == "__main__":
     import argparse
@@ -31,10 +59,10 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
-    parser.add_argument("--modelnet", default=20, type=int, help="ModelNet dimension.")
-    parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
-    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+    parser.add_argument("--batch_size", default=100, type=int, help="Batch size.")
+    parser.add_argument("--modelnet", default=32, type=int, help="ModelNet dimension.")
+    parser.add_argument("--epochs", default=25, type=int, help="Number of epochs.")
+    parser.add_argument("--threads", default=0, type=int, help="Maximum number of threads to use.")
     args = parser.parse_args()
 
     # Fix random seeds and number of threads
@@ -52,7 +80,6 @@ if __name__ == "__main__":
 
     # Load the data
     modelnet = ModelNet(args.modelnet)
-
     # Create the network and train
     network = Network(modelnet, args)
     network.train(modelnet, args)
