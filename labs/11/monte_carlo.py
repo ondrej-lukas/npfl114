8fea6fab297488a03b26c7096688a4a8f0271c2f
Ondrej Lukas <ondrej.lukas95@gmail.com>
2019-05-21 11:29:33 +0200

    merge issues

--- labs/11/monte_carlo.py
+++ labs/11/monte_carlo.py
@@ -2,6 +2,7 @@
 #bfc95faa-444e-11e9-b0fd-00505601122b
 #3da961ed-4364-11e9-b0fd-00505601122b
 import numpy as np
+
 import cart_pole_evaluator
 
 if __name__ == "__main__":
@@ -9,7 +10,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--episodes", default=1000, type=int, help="Training episodes.")
-    parser.add_argument("--epsilon", default=0.1, type=float, help="Exploration factor.")
+    parser.add_argument("--epsilon", default=0.2, type=float, help="Exploration factor.")
     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
     args = parser.parse_args()
 
@@ -30,7 +31,7 @@ if __name__ == "__main__":
 
     for _ in range(args.episodes):
         # Perform episode
-        state = env.reset(start_evaluate=False)
+        state = env.reset()
         states, actions, rewards = [], [], []
         while True:
             if args.render_each and env.episode > 0 and env.episode % args.render_each == 0:
@@ -59,11 +60,10 @@ if __name__ == "__main__":
         # TODO: Update Q and C
         G = 0
         for i in range(len(states)-1,-1, -1):
-            # print(i)
             G = G + rewards[i]
             C[states[i],actions[i]] = C[states[i],actions[i]] + 1
             Q[states[i], actions[i]] = Q[states[i],actions[i]] + (1/C[states[i],actions[i]]) * (G - Q[states[i],actions[i]])
-    
+
     # Final evaluation
     while True:
         state, done = env.reset(True), False
