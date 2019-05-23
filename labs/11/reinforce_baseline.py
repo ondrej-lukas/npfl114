cc19df682456cd727454cc45c2f4a1ed454094dc
Michaela Urbanovská <urbanm30@fel.cvut.cz>
2019-05-23 14:34:45 +0200

    furt nic...

--- labs/11/reinforce_baseline.py
+++ labs/11/reinforce_baseline.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", default=10, type=int, help="Number of episodes to train on.")
-    parser.add_argument("--episodes", default=1000, type=int, help="Training episodes.")
+    parser.add_argument("--episodes", default=1500, type=int, help="Training episodes.")
     parser.add_argument("--hidden_layer", default=64, type=int, help="Size of hidden layer.")
     parser.add_argument("--learning_rate", default=0.02, type=float, help="Learning rate.")
     parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
